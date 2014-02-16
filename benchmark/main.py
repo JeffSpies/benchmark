6a50e5afaafa86a888e04b26959f5a84abe2f523
Russel Winder <russel@winder.org.uk>
2014-02-16 09:04:35 +0000

    Change basestring to str for Python2 and 3 compatibility.

--- benchmark/main.py
+++ benchmark/main.py
@@ -11,13 +11,13 @@ import os
 import sys
 
 class BenchmarkProgram(object):
-    
+
     def __init__(self, module="__main__", **kwargs):
-        if isinstance(module, basestring):
+        if isinstance(module, str):
             self.module = __import__(module)
-        
+
         benchmarks = self.loadFromModule(self.module)
-        
+
         totalRuns = 0
         objects = []
         for obj in benchmarks:
@@ -25,21 +25,21 @@ class BenchmarkProgram(object):
             obj.run()
             objects.append(obj)
             totalRuns += obj.get_total_runs()
-        
+
         title = 'Benchmark Report'
         info = 'Each of the above %s runs were run in random, non-consecutive order by' % str(totalRuns)
         info += os.linesep
         info += '`benchmark` v' + __VERSION__ + ' (http://jspi.es/benchmark) with Python ' + platform.python_version()
         info += os.linesep
         info += '%s-%s-%s on %s' % (
-            platform.system(), 
-            platform.release(), 
-            platform.machine(), 
+            platform.system(),
+            platform.release(),
+            platform.machine(),
             time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
         ) + '.'
-        
+
         sys.stdout.write(self.printMarkdown(objects, title, info, **kwargs))
-    
+
     def printMarkdown(self, benchmarks, title, info, **kwargs):
         lines = ''
 
@@ -47,7 +47,7 @@ class BenchmarkProgram(object):
         lines += title
         lines += os.linesep + '='*len(title)
         lines += os.linesep*2
-        
+
         for obj in benchmarks:
             title = obj.label
             labelLength = len(title) if len(title) > 5 else 5
@@ -55,15 +55,15 @@ class BenchmarkProgram(object):
             lines += os.linesep
             lines += '-'*labelLength
             lines += os.linesep*2
-            
+
             lines += obj.get_table(**kwargs)
             lines += os.linesep*2
-        
+
         lines += info
         lines += os.linesep*2
 
         return lines
-        
+
     def loadFromModule(self, module):
         benchmarks = []
         for name in dir(module):
